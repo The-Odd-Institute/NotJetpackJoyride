81412f6e520455768a9b7e5093065a9dd18a753a
MiguelAyala25 <tlacuacheveloz25@gmail.com>
2024-04-22 23:25:02 -0700

    Touch controls added

---
+++ NotJetpackJoyrideProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float jumpTime;
+    [SerializeField] private float rocketForce;
+    [SerializeField] private Collider2D groundCheck;
+    [SerializeField] private float downwardForce;
+    [SerializeField] bool invert = false;
+
+    private Transform playerTransform;
+    private Rigidbody2D playerRigidbody;
+    private bool isOnGround = false;
+    private bool isJumping = false;
+    private int invertMod = 1;
+    private float currentJumpTime = 0;
+
+    void Start()
+    {
+        playerTransform = GetComponent<Transform>();
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        InvertHandler(); //sets invertMod to 1 or -1 to invert forces
+        //InputHandler();
+        TouchInputHandler();
+        JumpHandler();
+
+        if(!Input.GetKey(KeyCode.Space) && !isOnGround && !isJumping) // 'gravity' but not accelerating
+        {
+            playerRigidbody.AddForce(new Vector2(0, -invertMod * downwardForce * Time.deltaTime), ForceMode2D.Impulse);
+        }
+    }
+
+    private void InputHandler()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !invert) //player jump, gets higher more quickly
+        {
+            playerRigidbody.AddForce(new Vector2(0, jumpForce * invertMod), ForceMode2D.Force);
+            isJumping = true;
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumping = false;
+        }
+        else if (Input.GetKey(KeyCode.Space) && !isOnGround && !isJumping && currentJumpTime <= 0) //rocket upwards, not on the ground
+        {
+            playerRigidbody.AddForce(new Vector2(0, rocketForce * invertMod), ForceMode2D.Force);
+        }
+    }
+    private void TouchInputHandler()
+    {
+        if (Input.touchCount > 0)
+        {
+            Debug.Log("touch sensed");
+
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began && isOnGround && !invert)
+            {
+                playerRigidbody.AddForce(new Vector2(0, jumpForce * invertMod), ForceMode2D.Force);
+                isJumping = true;
+            }
+            else if (touch.phase == TouchPhase.Stationary|| touch.phase == TouchPhase.Moved)
+            {
+                if (!isOnGround && currentJumpTime < jumpTime)
+                {
+                    playerRigidbody.AddForce(new Vector2(0, rocketForce * invertMod), ForceMode2D.Force);
+                }
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                isJumping = false;
+                currentJumpTime = 0;
+            }
+        }
+    }
+
+
+    private void InvertHandler()
+    {
+        if (!invert)
+        {
+            invertMod = 1;
+            playerTransform.transform.rotation = Quaternion.Euler(0, 0, 0);
+            return;
+        }
+
+        invertMod = -1; // invert mod is just to flip the gravity and forces when inverting.
+        playerTransform.transform.rotation = Quaternion.Euler(0, 180, 180);
+        
+    }
+
+    private void JumpHandler()
+    {
+        if (isJumping)
+        {
+            currentJumpTime += Time.deltaTime;
+            if (currentJumpTime > jumpTime)
+            {
+                currentJumpTime = 0;
+                isJumping = false;
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == 6) // 6 is ground layer
+        {
+            isOnGround = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 6) // 6 is ground layer
+        {
+            isOnGround = false;
+        }
+    }
+}
