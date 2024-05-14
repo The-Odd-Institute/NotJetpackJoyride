f42ee3b1a990cad9f3f6fc36ca66d708717a2e8c
LeahTaurisano <leahtaurisano@gmail.com>
2024-05-14 16:52:31 -0700

    Fixed Coins Finally!

--- NotJetpackJoyrideProject/Assets/Scripts/Coin/AnimationTriggerBox.cs
+++ NotJetpackJoyrideProject/Assets/Scripts/Coin/AnimationTriggerBox.cs
@@ -4,8 +4,13 @@ public class AnimationTriggerBox : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
+        Debug.Log("Insoide tyhe funct");
+         
         if (collision.gameObject.TryGetComponent<PrefabCoin>(out var coin))
         {
+            Debug.Log("Try worked");
+
             coin.SetRunAnimationTrue();
         }
     }
