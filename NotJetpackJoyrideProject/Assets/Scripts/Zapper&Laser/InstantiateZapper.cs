e063cb27ad9ee14a2e6f97a0faf42bebfe4af1b2
NicholasOkovic <139954443+NicholasOkovic@users.noreply.github.com>
2024-05-14 16:48:27 -0700

    Zappers fixed and added rotating zapper

--- NotJetpackJoyrideProject/Assets/Scripts/Zapper&Laser/InstantiateZapper.cs
+++ NotJetpackJoyrideProject/Assets/Scripts/Zapper&Laser/InstantiateZapper.cs
@@ -7,15 +7,16 @@ public class InstantiateZapper : MonoBehaviour
     private GameObject newZapper = null;
     public GameObject zapper;
     private GameObject endZapper;
-   // private GameObject startZapper;
-    //private GameObject centerPivot;
+    private GameObject startZapper;
+    private GameObject centerPivot;
     private GameObject boxCollider;
 
-    //[SerializeField] private bool rotatingZapper = false;
+    [SerializeField] private bool rotatingZapper = false;
 
     //used for adjusting the length of zapper
     [SerializeField] private float minSize;
     [SerializeField] private float maxSize;
+    //DO NOT SET MIN SIZE TO 0, It will break the box collider
 
     //used for adjusting the rotation of zapper
     [SerializeField] private float minRot;
@@ -25,19 +26,12 @@ public class InstantiateZapper : MonoBehaviour
     float zapper_Rotation;
 
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     private void Update()
     {
-        //if (Input.GetKeyDown(KeyCode.Z))
-        //{
-        //    InstantiateObject(1);
-        //}
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            InstantiateObject(1);
+        }
     }
 
     public void InstantiateObject(int offset)
@@ -50,39 +44,41 @@ public class InstantiateZapper : MonoBehaviour
         //random rotation
 
         zapper_Length = Random.Range(minSize, maxSize);
-        zapper_Rotation = Random.Range(minSize, maxSize);
+        zapper_Rotation = Random.Range(minRot, maxRot);
+
 
         //instantiate
-        newZapper = Instantiate(zapper, new Vector2(gameObject.transform.position.x, offset), Quaternion.Euler(0, 0, zapper_Rotation));
-        newZapper.GetComponent<MoveLeft>().IsMoving = true;
-
+        newZapper = Instantiate(zapper, new Vector2(gameObject.transform.position.x, offset), Quaternion.identity);
 
         //get its children
-        
         endZapper = newZapper.transform.GetChild(1).gameObject;
         boxCollider = newZapper.transform.GetChild(2).gameObject;
 
-        //length of how far away the end of the zapper will be
-        //needs formula for rotation, not following box collider nor center pivot
         endZapper.transform.position = new Vector2(zapper_Length, endZapper.transform.position.y);
-        //endZapper.transform.position = new Vector2(endZapper.transform.position.x + zapper_Length * Mathf.Cos(zapper_Rotation), endZapper.transform.position.y + zapper_Length * Mathf.Sin(zapper_Rotation));
 
+        
+        if (rotatingZapper)
+        {
+            //rotating script
+            newZapper.GetComponent<Rotate>().IsRotating = true;
+
+            //change laser color
+            startZapper = newZapper.transform.GetChild(0).gameObject;
+            centerPivot = newZapper.transform.GetChild(3).gameObject;
+            centerPivot.transform.position = new Vector2(zapper_Length/2, centerPivot.transform.position.y);
+            startZapper.GetComponent<LineRenderer>().startColor = Color.red;
+        }
+
+
+
+        newZapper.GetComponent<MoveLeft>().IsMoving = true;
+
+       
         //scales box collider depending on zapper length
         boxCollider.transform.localScale = new Vector3(zapper_Length, boxCollider.transform.localScale.y, boxCollider.transform.localScale.z);
 
-
-
-        //if (rotatingZapper)
-        //{
-        //    //rotating script
-        //    newZapper.GetComponent<Rotate>().IsRotating = true;
-
-        //    //change laser color
-        //    startZapper = newZapper.transform.GetChild(0).gameObject;
-        //    centerPivot = newZapper.transform.GetChild(3).gameObject;
-        //    centerPivot.transform.localScale = new Vector3(zapper_Length, centerPivot.transform.localScale.y, centerPivot.transform.localScale.z);
-        //    startZapper.GetComponent<LineRenderer>().startColor = Color.red;
-        //}
+        //rotate the zapper last
+        newZapper.transform.Rotate(0, 0, zapper_Rotation);
 
     }
 
