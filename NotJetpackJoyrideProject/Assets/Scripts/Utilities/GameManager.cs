dd3bcedf612019b2c4d45c8245c5e6fc8092d641
SleepingDragon <126519201+SleepingDragon0@users.noreply.github.com>
2024-05-14 16:41:31 -0700

    updated ui

--- NotJetpackJoyrideProject/Assets/Scripts/Utilities/GameManager.cs
+++ NotJetpackJoyrideProject/Assets/Scripts/Utilities/GameManager.cs
@@ -9,6 +9,8 @@ public  class GameManager : MonoBehaviour
   
     public void LoadDeathScreen()
     {
+        //var temp = GetComponent<ScoreManager>();
+        //temp.OnGameOver();
         SceneManager.LoadScene(2);
     }
     public  void CaptureScreenshot(string filename, int superSize)
