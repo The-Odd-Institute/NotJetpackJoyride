7a66936ce0f2c43122d8b515e09e812c87f28acd
MiguelAyala25 <tlacuacheveloz25@gmail.com>
2024-05-24 23:27:41 -0700

    lights

--- NotJetpackJoyrideProject/Assets/LightManipulator.cs
+++ NotJetpackJoyrideProject/Assets/LightManipulator.cs
@@ -16,18 +16,5 @@ public class LightManipulator : MonoBehaviour
     void FixedUpdate()
     {
 
-        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
-        Debug.DrawRay(transform.position, forward, Color.green);
-
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
-
-        // If it hits something...
-        if (hit.collider != null)
-        {
-
-            float distance = Mathf.Abs(hit.point.y - transform.position.y);
-
-            rb2D.AddForce(Vector3.up * hit.point.x);
-        }
     }
 }
