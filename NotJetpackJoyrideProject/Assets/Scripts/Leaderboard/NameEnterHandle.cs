c00632486541ce5dcd7df8637d8e579dd1291214
Phi Bao <112589448+PhiBeo@users.noreply.github.com>
2024-06-04 14:36:42 -0700

    Naming Character Limit

--- NotJetpackJoyrideProject/Assets/Scripts/Leaderboard/NameEnterHandle.cs
+++ NotJetpackJoyrideProject/Assets/Scripts/Leaderboard/NameEnterHandle.cs
@@ -6,26 +6,30 @@ using UnityEngine.UI;
 
 public class NameEnterHandle : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI characterLimitText;
     [SerializeField] private TMP_InputField inputField;
     [SerializeField] private Button submitButton;
     [SerializeField] private UnityServiceInitialize unityService;
+    [SerializeField] private int numberOfNameCharacterDisplay = 12;
 
     void Start()
     {
         submitButton.interactable = !(inputField.text == "");
+        characterLimitText.text = "Character Limit: " + numberOfNameCharacterDisplay;
     }
 
-    // Update is called once per frame
     void Update()
-    {
-        if (inputField.text != "")
-            submitButton.interactable = true;
-        else
+    {  
+        if(inputField.text == "" || inputField.text.Length > numberOfNameCharacterDisplay)
             submitButton.interactable = false;
+        else if (inputField.text != "")
+            submitButton.interactable = true;
     }
 
     public void ConfirmUpdateName()
     {
+        if (inputField.text.Length <= 0) return;
+
         unityService.Rename(inputField.text);
     }
 }
